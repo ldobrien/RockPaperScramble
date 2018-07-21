c415702d4b4dfc0f5f1b7b1c5cbbc28df27b8a2f
o6g7 <o6g7@ugrad.cs.ubc.ca>
2018-07-21 14:30:00 -0700

    heroku

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use('/api', apiRoutes);
 
 
 // start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+app.listen(process.env.PORT || 8080, function() {
+  console.log('Express server is up and running!');
 });
